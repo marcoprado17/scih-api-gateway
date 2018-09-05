2ecba326b4593e3a1c806357a75e76507ff122b4
Marco Aurélio Prado <marco.pdsv@gmail.com>
2018-09-05 16:02:10 -0300

    Adding http proxy

---
+++ routes.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+const httpProxy = require('express-http-proxy');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+const userServiceProxy = httpProxy('http://localhost:3001');
+
+// Proxy request
+router.get('/users', (req, res, next) => {
+  userServiceProxy(req, res, next);
+})
+
+
+module.exports = router;
