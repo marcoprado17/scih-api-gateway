5512ce8abe9f6ebb08f6d7ffd5ef367f575f05e4
Marco Aurélio Prado <marco.pdsv@gmail.com>
2018-09-06 20:55:46 -0300

    Adding post and get forward rule for gps-data

--- routes.js
+++ routes.js
@@ -4,20 +4,23 @@ const httpProxy = require('express-http-proxy');
 
 /* Returning 200 on / to serve as health check to ingress: https://cloud.google.com/kubernetes-engine/docs/tutorials/http-balancer#remarks */
 router.get('/', function(req, res, next) {
-    res.sendStatus(200)
-  });
+  res.sendStatus(200)
+});
 
 /* GET home page. */
 router.get('/api', function(req, res, next) {
-  res.send("Home page da api!!! Please, aparece na página =)");
+  res.send("Home page da api!!!");
 });
 
+/* gps-sercice rules */
 const gpsServiceProxy = httpProxy('http://gps-service');
 
-// Proxy request
-router.get('/api/accounts/:accountId/gps-data', (req, res, next) => {
+router.post('/api/accounts/:accountId/contracts/:contractId/gps-data', (req, res, next) => {
     gpsServiceProxy(req, res, next);
 })
 
+router.get('/api/accounts/:accountId/contracts/:contractId/gps-data', (req, res, next) => {
+  gpsServiceProxy(req, res, next);
+})
 
 module.exports = router;
