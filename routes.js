e420d7265f4c333fd1e7c07c9a39ded35428217d
Marco Aurélio Prado <marco.pdsv@gmail.com>
2018-09-06 13:12:48 -0300

    ..

--- routes.js
+++ routes.js
@@ -2,6 +2,11 @@ var express = require('express');
 var router = express.Router();
 const httpProxy = require('express-http-proxy');
 
+/* Returning 200 on / to serve as health check to ingress:  */
+router.get('/', function(req, res, next) {
+    res.sendStatus(200)
+  });
+
 /* GET home page. */
 router.get('/api', function(req, res, next) {
   res.send("Home page da api!!!");
@@ -10,7 +15,7 @@ router.get('/api', function(req, res, next) {
 const gpsServiceProxy = httpProxy('http://gps-service');
 
 // Proxy request
-router.get('/api/account/:accountId/gps-data', (req, res, next) => {
+router.get('/api/accounts/:accountId/gps-data', (req, res, next) => {
     gpsServiceProxy(req, res, next);
 })
 
