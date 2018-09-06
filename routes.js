84e699cfdaaa0c911b83b95f82edde359621049c
Marco Aurélio Prado <marco.pdsv@gmail.com>
2018-09-06 13:29:23 -0300

    ...

--- routes.js
+++ routes.js
@@ -2,14 +2,14 @@ var express = require('express');
 var router = express.Router();
 const httpProxy = require('express-http-proxy');
 
-/* Returning 200 on / to serve as health check to ingress:  */
+/* Returning 200 on / to serve as health check to ingress: https://cloud.google.com/kubernetes-engine/docs/tutorials/http-balancer#remarks */
 router.get('/', function(req, res, next) {
     res.sendStatus(200)
   });
 
 /* GET home page. */
 router.get('/api', function(req, res, next) {
-  res.send("Home page da api!!!");
+  res.send("Home page da api!!! Please, aparece na página =)");
 });
 
 const gpsServiceProxy = httpProxy('http://gps-service');
